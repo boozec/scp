246369828ecdaf879923b19ff222881cbe6c3953
Santo Cariotti <santo@dcariotti.me>
2024-12-27 22:50:35 +0100

    Use two workers

--- scripts/02-dataproc-create-cluster.sh
+++ scripts/02-dataproc-create-cluster.sh
@@ -4,7 +4,8 @@ gcloud dataproc clusters create ${CLUSTER} \
     --project=${PROJECT} \
     --region=${REGION} \
     --service-account=${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com \
+    --num-workers 2 \
     --master-boot-disk-size 240 \
     --worker-boot-disk-size 240 \
-    --num-workers 1 \
-    --single-node
+    --worker-machine-type n1-standard-2 \
+    --master-machine-type n1-standard-2
