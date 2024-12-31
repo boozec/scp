861400563abee500682bea89161eabfcb86a3e60
Santo Cariotti <santo@dcariotti.me>
2024-12-31 18:32:03 +0100

    Read path automatically

--- scripts/01-create-bucket.sh
+++ scripts/01-create-bucket.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-read -p "Enter 'order_products.csv' path: " path
+path=$(find . -name 'order_products.csv' -print -quit)
+
+if [ -z "$path" ]; then
+    read -p "Enter 'order_products.csv' path: " path
+    if [ -z "$path" ] || [ ! -e "$path" ]; then
+        echo ">>>> Path not found..."
+        exit 1
+    fi
+fi
 
 gcloud storage buckets create gs://$BUCKET_NAME --location=eu
 gcloud storage buckets add-iam-policy-binding gs://${BUCKET_NAME} \
