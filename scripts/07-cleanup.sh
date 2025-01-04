8094e6a64f166863fbeb445457364616f8bd8a7e
Santo Cariotti <santo@dcariotti.me>
2025-01-04 13:56:44 +0100

    Remove all buckets

--- scripts/07-cleanup.sh
+++ scripts/07-cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcloud storage rm -r gs://${BUCKET_NAME}
+gcloud storage rm -r gs://
 gcloud dataproc clusters delete ${CLUSTER} --region=${REGION}
 gcloud iam service-accounts delete ${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com
